34e70c53f6dc2a6f1324458e4d549c76606925da
HyunHae <24hyunhae7@gmail.com>
2025-11-06 13:04:12 -0800

    new commit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -6,4 +7,7 @@ int main()
 {
     cout << "Helllo world" << endl;
     cout << "O.o" << endl;
+
+    string s = "abc";
+    cout << s << endl;
 }
